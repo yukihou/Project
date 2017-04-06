66bd4af442637ae7a6bc370dd33b9fce53cba628
yukihou <1262695970@qq.com>
2017-04-06 20:42:54 +0800

    first commit

---
+++ foo.css
@@ -0,0 +1,35 @@
+* {
+    /* css reset */
+    margin: 0;
+    padding: 0;
+}
+body{
+    background-image: url(http://cdn.duitang.com/uploads/item/201202/04/20120204195806_G4srY.jpg);
+    background-repeat: repeat;
+}
+#mian{
+    color: lightcoral;
+    font-family: Fantasy;
+    font-size: 77px;
+    text-align: center;   
+    text-decoration: blink;
+    margin-top:200px;
+}
+.first{
+    color: white;
+    background-color: lightcoral;
+    font-family: 'Times New Roman', Times, serif;
+    font-size: 30px;
+    text-decoration: none;
+    margin-left:450px;
+    margin-top: 300px;
+}   
+.second{
+    color: white;
+    background-color: lightcoral;
+    font-family: 'Times New Roman', Times, serif;
+    font-size: 30px;
+    text-decoration: none;
+    margin-left:220px;
+    margin-top: 300px;
+}
